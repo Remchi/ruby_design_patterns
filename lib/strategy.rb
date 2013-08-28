e6c0228a70bce9edb5c388405a6850c5a85db654
Rem Zolotykh <remchi@gmail.com>
2013-08-28 16:25:56 +0300

    Strategy Pattern to delegate stats printing

--- lib/strategy.rb
+++ lib/strategy.rb
@@ -1,20 +1,28 @@
 class Hero
   attr_reader :damage, :health, :skills
+  attr_accessor :printer
 
-  def initialize
+  def initialize(printer)
     @damage = 10
     @health = 5
+    @printer = printer
 
     @skills = [:stealth, :driving, :intimidation]
   end
 
   def print_stats(stats = nil)
-    if stats == :skills
-      skills.inject("") { |result, skill| result + skill.to_s.capitalize + "\n"}
-    else
-      "Damage: #{damage}\nHealth: #{health}"
-    end
+    printer.print(self)
   end
+end
 
+class BattleStats
+  def print(context)
+    "Damage: #{context.damage}\nHealth: #{context.health}"
+  end
+end
 
+class SkillsStats
+  def print(context)
+    context.skills.inject("") { |result, skill| result + skill.to_s.capitalize + "\n"}
+  end
 end
--- spec/strategy_spec.rb
+++ spec/strategy_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/strategy'
 describe "Strategy Pattern" do
 
   describe Hero do
-    let(:hero) { Hero.new }
+    let(:hero) { Hero.new BattleStats.new }
 
     it "has damage" do
       expect(hero.damage).to eq(10)
@@ -20,6 +20,7 @@ describe "Strategy Pattern" do
       end
 
       it "prints skills" do
+        hero.printer = SkillsStats.new
         expect(hero.print_stats(:skills)).to eq("Stealth\nDriving\nIntimidation\n")
       end
     end
