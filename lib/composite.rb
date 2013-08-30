4366c667fbf12f57a58f130560d81e234116be92
Rem Zolotykh <remchi@gmail.com>
2013-08-30 22:25:04 +0300

    Refactor to CompositeQuest

--- lib/composite.rb
+++ lib/composite.rb
@@ -1,4 +1,4 @@
-class MegaQuest
+class CompositeQuest
 
   def initialize
     @tasks = []
@@ -14,20 +14,10 @@ class MegaQuest
 
 end
 
-class Quest
+class MegaQuest < CompositeQuest
+end
 
-  def initialize
-    @tasks = []
-  end
-
-  def add(task)
-    @tasks << task
-  end
-
-  def reward
-    @tasks.inject(0) { |sum, task| sum += task.reward }
-  end
-
+class Quest < CompositeQuest
 end
 
 
