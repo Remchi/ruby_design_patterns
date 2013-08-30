593e10f3cd835c5d178a2483d63563153535e8ad
Rem Zolotykh <remchi@gmail.com>
2013-08-30 22:20:47 +0300

    Quest composed from tasks

--- lib/composite.rb
+++ lib/composite.rb
@@ -1,3 +1,20 @@
+class Quest
+
+  def initialize
+    @tasks = []
+  end
+
+  def add(task)
+    @tasks << task
+  end
+
+  def reward
+    @tasks.inject(0) { |sum, task| sum += task.reward }
+  end
+
+end
+
+
 class Encounter
   attr_reader :reward
 
--- spec/composite_spec.rb
+++ spec/composite_spec.rb
@@ -3,6 +3,18 @@ require_relative '../lib/composite'
 
 describe "Composite Pattern" do
 
+  describe Quest do
+    context "consists from Encounter and Diplomacy" do
+      it "has reward of 600" do
+        quest = Quest.new
+        quest.add(Encounter.new)
+        quest.add(Diplomacy.new)
+
+        expect(quest.reward).to eq(600)
+      end
+    end
+  end
+
   describe Encounter do
     it "has experience reward of 100" do
       expect(Encounter.new.reward).to eq(100)
