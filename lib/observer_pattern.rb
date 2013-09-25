7426c8f64df7d5f5637335d3c138456c9d4acb33
Rem Zolotykh <remchi@gmail.com>
2013-09-25 23:22:46 +0300

    Activate curse - smelly approach

--- lib/observer_pattern.rb
+++ lib/observer_pattern.rb
@@ -2,18 +2,29 @@ class Tile
 
   def initialize(attrs = {})
     @cursed = attrs.fetch(:cursed, false)
+    @hero = attrs.fetch(:hero, nil)
   end
 
   def cursed?
     @cursed
   end
 
+  def activate_curse
+    @hero.damage(4)
+  end
+
 end
 
 class Hero
+  attr_reader :health
 
   def initialize
     @cursed = false
+    @health = 10
+  end
+
+  def damage(hit)
+    @health -= hit
   end
 
   def cursed?
--- spec/observer_pattern_spec.rb
+++ spec/observer_pattern_spec.rb
@@ -4,8 +4,9 @@ require_relative '../lib/observer_pattern'
 describe "Observer Pattern" do
 
   describe Hero do
+    let(:hero) { Hero.new }
+
     it "is cursed when discovers cursed Tile" do
-      hero = Hero.new
       tile = Tile.new cursed: true
       hero.discover(tile)
 
@@ -13,15 +14,31 @@ describe "Observer Pattern" do
     end
 
     it "is not cursed when discovers simple Tile without curse" do
-      hero = Hero.new
       tile = Tile.new
       hero.discover(tile)
 
       expect(hero.cursed?).to be_false
     end
+
+    it "has default health equal to 10" do
+      expect(hero.health).to eq(10)
+    end
+    it "can be damaged" do
+      hero.damage(6)
+      expect(hero.health).to eq(4)
+    end
   end
 
   describe Tile do
+
+    it "activates curse" do
+      hero = Hero.new
+      tile = Tile.new cursed: true, hero: hero
+
+      tile.activate_curse
+      expect(hero.health).to eq(6)
+    end
+
     it "is not cursed by default" do
       tile = Tile.new
       expect(tile.cursed?).to be_false
