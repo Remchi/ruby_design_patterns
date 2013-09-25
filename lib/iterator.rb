f652fe450e14ba9ae8b8b06f5ac966d763a4b7d7
Rem Zolotykh <remchi@gmail.com>
2013-09-25 22:24:49 +0300

    Inventory class

--- lib/iterator.rb
+++ lib/iterator.rb
@@ -1,8 +1,17 @@
 class Inventory
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def add(item)
+    @items << item
+  end
 end
 
 class Item
-  attr_reader :cost
+  attr_accessor :cost
 
   def initialize
     @cost = 0
--- spec/iterator_spec.rb
+++ spec/iterator_spec.rb
@@ -4,6 +4,21 @@ require_relative '../lib/iterator'
 describe "Iterator Pattern" do
 
   describe Inventory do
+
+    it "adds items to its collection" do
+      item1 = Item.new
+      item1.cost = 20
+
+      item2 = Item.new
+      item2.cost = 10
+
+      inventory = Inventory.new
+      inventory.add(item1)
+      inventory.add(item2)
+
+      expect(inventory.items.size).to eq(2)
+    end
+
   end
 
   describe Item do
