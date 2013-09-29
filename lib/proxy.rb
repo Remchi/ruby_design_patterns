71043f07bd7ace1066b1539b91cc9c7e3b982f89
Rem Zolotykh <remchi@gmail.com>
2013-09-29 20:47:53 +0300

    Authorization with proxy

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,12 +1,32 @@
 require 'forwardable'
 
+class Hero
+  attr_accessor :keywords
+
+  def initialize
+    @keywords = []
+  end
+end
+
 class ComputerProxy
   extend Forwardable
 
-  def_delegators :@real_object, :add, :execute
+  def_delegators :@real_object, :add
 
-  def initialize(real_object)
+  def initialize(real_object, hero)
     @real_object = real_object
+    @hero = hero
+  end
+
+  def execute
+    check_access
+    @real_object.execute
+  end
+
+  def check_access
+    unless @hero.keywords.include?(:computer)
+      raise 'You have no access'
+    end
   end
 
 end
--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -4,12 +4,14 @@ require_relative '../lib/proxy'
 describe "Proxy Pattern" do
 
   it "delegates all functionality to the real object" do
+    hero = Hero.new
     computer = double("computer", queue: [], add: [], execute: true)
-    proxy = ComputerProxy.new(computer)
+    proxy = ComputerProxy.new(computer, hero)
 
     computer.should_receive(:add)
     proxy.add(double("command"))
 
+    hero.keywords << :computer
     computer.should_receive(:execute)
     proxy.execute
   end
