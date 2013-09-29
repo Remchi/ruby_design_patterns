22eca48e400c65bb83112a5d844be767c2d2cbfc
Rem Zolotykh <remchi@gmail.com>
2013-09-29 16:28:15 +0300

    Magic and masterpiece items

--- lib/decorator.rb
+++ lib/decorator.rb
@@ -1,9 +1,19 @@
 class Item
   attr_reader :price, :description
 
-  def initialize
+  def initialize(is_magic = false, is_masterpiece = false)
     @price = 10
     @description = "Item."
+
+    if is_magic
+      @price *= 3
+      @description += "Magic."
+    end
+
+    if is_masterpiece
+      @price *= 2
+      @description += "Masterpiece."
+    end
   end
 
   def use
--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -6,6 +6,39 @@ describe "Decorator Pattern" do
   describe Item do
     let(:common_item) { Item.new }
 
+    context "magic item" do
+      let(:magic_item) { Item.new true}
+
+      it "three times more expensive than common item" do
+        expect(magic_item.price).to eq(common_item.price * 3)
+      end
+      it "has addition do description" do
+        expect(magic_item.description).to eq(common_item.description + "Magic.")
+      end
+    end
+
+    context "magic masterpiece item" do
+      let(:full_item) { Item.new true, true }
+
+      it "six times more expensive than common item" do
+        expect(full_item.price).to eq(common_item.price * 6)
+      end
+      it "has addition do description" do
+        expect(full_item.description).to eq(common_item.description + "Magic.Masterpiece.")
+      end
+    end
+
+    context "masterpiece item" do
+      let(:masterpiece_item) { Item.new false, true }
+
+      it "two times more expensive than common item" do
+        expect(masterpiece_item.price).to eq(common_item.price * 2)
+      end
+      it "has addition do description" do
+        expect(masterpiece_item.description).to eq(common_item.description + "Masterpiece.")
+      end
+    end
+
     context "common item" do
 
       it "has price" do
