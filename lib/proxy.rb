af1050aa13d1ab3c0061267cb0fe5aaa28be6ee9
Rem Zolotykh <remchi@gmail.com>
2013-09-29 20:51:33 +0300

    Lazy initialization

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -11,22 +11,25 @@ end
 class ComputerProxy
   extend Forwardable
 
-  def_delegators :@real_object, :add
+  def_delegators :real_object, :add
 
-  def initialize(real_object, hero)
-    @real_object = real_object
+  def initialize(hero)
     @hero = hero
   end
 
   def execute
     check_access
-    @real_object.execute
+    real_object.execute
   end
 
   def check_access
     unless @hero.keywords.include?(:computer)
       raise 'You have no access'
     end
+  end
+
+  def real_object
+    @real_object || (@real_object = Computer.new)
   end
 
 end
--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -6,7 +6,8 @@ describe "Proxy Pattern" do
   it "delegates all functionality to the real object" do
     hero = Hero.new
     computer = double("computer", queue: [], add: [], execute: true)
-    proxy = ComputerProxy.new(computer, hero)
+    Computer.stub(:new).and_return(computer)
+    proxy = ComputerProxy.new(hero)
 
     computer.should_receive(:add)
     proxy.add(double("command"))
