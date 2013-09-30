c06cd51b434c7a494b01b5bfee0d3763a7d8cec4
Rem Zolotykh <remchi@gmail.com>
2013-09-30 16:33:10 +0300

    Builder board construction

---
+++ lib/builder.rb
@@ -0,0 +1,66 @@
+class BoardBuilder
+  def initialize(width, height, difficulty)
+    @board = Board.new
+    @board.width = width
+    @board.height = height
+    @board.difficulty = difficulty
+    @board.tiles = []
+    @board.monsters = []
+  end
+
+  def add_tiles(type, number)
+    message = "create_#{type}_tiles"
+    @board.tiles.concat(TileFactory.send(message, number))
+  end
+  def add_monsters(type, number)
+    message = "create_#{type}s"
+    @board.monsters.concat(MonsterFactory.send(message, number))
+  end
+
+  def board
+    @board
+  end
+end
+
+class Board
+  attr_accessor :width, :height, :difficulty, :tiles, :monsters
+
+  def initialize
+    @width = width
+    @height = height
+    @difficulty = difficulty
+    @tiles = tiles
+    @monsters = monsters
+  end
+end
+
+class PlainTile; end
+class TrapTile; end
+class Orc; end
+class Goblin; end
+
+class TileFactory
+  def self.create_plain_tiles(number)
+    tiles = []
+    number.times { tiles << PlainTile.new }
+    tiles
+  end
+  def self.create_trap_tiles(number)
+    tiles = []
+    number.times { tiles << TrapTile.new }
+    tiles
+  end
+end
+
+class MonsterFactory
+  def self.create_orcs(number)
+    monsters = []
+    number.times { monsters << Orc.new }
+    monsters
+  end
+  def self.create_goblins(number)
+    monsters = []
+    number.times { monsters << Goblin.new }
+    monsters
+  end
+end
---
+++ spec/builder_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require_relative '../lib/builder'
+
+describe "Builder Pattern" do
+
+  it "creates board" do
+    pending
+    tiles = []
+    tiles << TileFactory.create_plain_tiles(5)
+    tiles << TileFactory.create_trap_tiles(5)
+
+    monsters = []
+    monsters << MonsterFactory.create_orcs(3)
+    monsters << MonsterFactory.create_goblins(5)
+
+    board = Board.new(2, 5, :easy, tiles, monsters)
+
+    expect(board.width).to eq(2)
+    expect(board.height).to eq(5)
+    expect(board.difficulty).to eq(:easy)
+    expect(board.tiles).to eq(tiles)
+    expect(board.monsters).to eq(monsters)
+  end
+
+  it "uses builder to create board" do
+    builder = BoardBuilder.new(2, 5, :easy)
+    builder.add_tiles(:plain, 5)
+    builder.add_tiles(:trap, 5)
+    builder.add_monsters(:orc, 3)
+    builder.add_monsters(:goblin, 5)
+
+    board = builder.board
+
+    expect(board.width).to eq(2)
+    expect(board.height).to eq(5)
+    expect(board.difficulty).to eq(:easy)
+    expect(board.tiles.size).to eq(10)
+    expect(board.monsters.size).to eq(8)
+  end
+
+end
